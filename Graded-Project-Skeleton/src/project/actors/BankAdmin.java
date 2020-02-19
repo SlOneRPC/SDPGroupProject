1265e50dc9b7b2eddbab8e34902afeebaf1b489c
diego <diego@Diego-Main-PC.home>
2020-02-19 20:11:09 +0000

    Admin things

--- Graded-Project-Skeleton/src/project/actors/BankAdmin.java
+++ Graded-Project-Skeleton/src/project/actors/BankAdmin.java
@@ -1,5 +1,16 @@
 package project.actors;
 
+import java.util.List;
+
+import gradedGroupProject.nonPrincipledDesign.v1.BankClient;
+
 public class BankAdmin {
 
+	public String name = "Y";
+
+	public List<BankClient> bankClientsToVerify;
+	public List<Integer> accountNumbersToVerify;
+	
+	public BankAdmin() {}
+	
 }
