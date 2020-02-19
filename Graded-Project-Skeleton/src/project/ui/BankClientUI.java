413abc5cb903d85eb186592b1c3bada4aba9f080
DevUser <t>
2020-02-19 20:22:05 +0000

    TEST

--- Graded-Project-Skeleton/src/project/ui/BankClientUI.java
+++ Graded-Project-Skeleton/src/project/ui/BankClientUI.java
@@ -10,32 +10,30 @@ import java.util.Date;
 import java.util.List;
 
 import project.actors.BankClient;
+import project.utilities.StdInput;
 
 public class BankClientUI {
 	
 	public static void main( String[] args ) throws ParseException {
 
 		List<BankClient> bankClients = new ArrayList<>();
-
 		String adminName = "X", employeeName = "Y";
-
-
+		StdInput input = new StdInput();
+		
 		while(true){
-
 			print(bankClients);
 
 			System.out.println( "\n0. Exit" );
 			System.out.println( "1. Register" ); //--> Create Profile --> Create Account --> Create Further Accounts
 			System.out.println( "4. Login" );
-			String choice = read( "choice" );
-
+			String choice = input.read( "choice" );
 
 			if( choice.equals( "0" ) ) break;
 
 			else if( choice.equals( "1" ) ) {
 
-				String username = read( "username" );
-				String password = read( "password" );
+				String username = input.read( "username" );
+				String password = input.read( "password" );
 
 
 				BankClient bankClient = new BankClient( username, password );
@@ -43,11 +41,11 @@ public class BankClientUI {
 				bankClients.add( bankClient );
 
 
-				String name = read( "name" );
-				String address = read( "address" );
+				String name = input.read( "name" );
+				String address = input.read( "address" );
 
 				Date birthDate = null;
-				try { birthDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse( read( "birthDate" ) ); }
+				try { birthDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse( input.read( "birthDate" ) ); }
 				catch( ParseException ex ){ ex.printStackTrace(); }
 
 				bankClient.createProfile( name, address, birthDate );
@@ -55,10 +53,9 @@ public class BankClientUI {
 
 				while( true ){
 
-					String accountType = read( "account type (primary, savings)" );
+					String accountType = input.read( "account type (primary, savings)" );
 
 					int accountNumber = bankClient.addAccount( accountType );
-
 					
 					boolean verified = bankClient.askForVerification( bankClient.clientID, accountNumber, adminName ); //TODO: contact bank admin
 
@@ -67,7 +64,7 @@ public class BankClientUI {
 
 					System.out.println( "\n0. NO extra account" );
 					System.out.println( "1. Extra account" );
-					String choice2 = read( "choice" );
+					String choice2 = input.read( "choice" );
 
 					if( choice2.equals( "0" ) ) break;
 				}
@@ -75,8 +72,8 @@ public class BankClientUI {
 
 			else if( choice.equals( "2" ) ) {
 
-				String username = read( "username" );
-				String password = read( "password" );
+				String username = input.read( "username" );
+				String password = input.read( "password" );
 
 
 				int pos = 0;
@@ -89,7 +86,7 @@ public class BankClientUI {
 
 					bankClients.get( pos ).toPrint();
 
-					int accountNumber = Integer.parseInt( read( "account number" ) );
+					int accountNumber = Integer.parseInt(input.read( "account number" ));
 
 					bankClients.get( pos ).toPrintAccount( accountNumber );
 
@@ -99,20 +96,20 @@ public class BankClientUI {
 					System.out.println( "6. Delete Bank Account" );
 					System.out.println( "7. Money transfer" );
 					System.out.println( "8. Book Appoinment" );
-					choice = read( "choice" );
+					choice = input.read( "choice" );
 
 
 					if( choice.equals( "5" ) ) {
 
-						String name = read( "new name" );
-						String address = read( "new address" );
+						String name = input.read( "new name" );
+						String address = input.read( "new address" );
 
 						Date birthDate = null;
-						try { birthDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse( read( "new birthDate" ) ); }
+						try { birthDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse(input.read( "new birthDate" )); }
 						catch( ParseException ex ){ ex.printStackTrace(); }
 
-						username = read( "new username" );
-						password = read( "new password" );
+						username = input.read( "new username" );
+						password = input.read( "new password" );
 
 						bankClients.get( pos ).changeClientDetails( name, address, birthDate, username, password );
 					}
@@ -121,7 +118,7 @@ public class BankClientUI {
 
 						bankClients.get( pos ).printAccounts();
 
-						accountNumber = Integer.parseInt( read( "account number" ) );
+						accountNumber = Integer.parseInt( input.read( "account number" ) );
 
 						bankClients.get( pos ).deleteAccount( accountNumber );
 
@@ -132,9 +129,9 @@ public class BankClientUI {
 
 						bankClients.get( pos ).printAccounts();
 
-						int fromAccountNumber = Integer.parseInt( read( "from account number" ) );
-						int toAccountNumber = Integer.parseInt( read( "to account number" ) );
-						double amount = Integer.parseInt( read( "amount" ) );
+						int fromAccountNumber = Integer.parseInt( input.read( "from account number" ) );
+						int toAccountNumber = Integer.parseInt(input.read( "to account number" ) );
+						double amount = Integer.parseInt(input.read( "amount" ) );
 
 						bankClients.get( pos ).transfer( fromAccountNumber, toAccountNumber, amount );
 					}
@@ -142,7 +139,7 @@ public class BankClientUI {
 					else if( choice.equals( "8" ) ) {
 
 						Date appointmentDate = null;
-						try { appointmentDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse( read( "apppoinment date" ) ); }
+						try { appointmentDate = new SimpleDateFormat( "dd/MM/yyyy" ).parse( input.read( "apppoinment date" ) ); }
 						catch( ParseException ex ){ ex.printStackTrace(); }
 
 
@@ -155,28 +152,6 @@ public class BankClientUI {
 				}
 			}
 		}
-	}
-	
-	public static String read( String label ) {
-
-		System.out.println( "\nProvide your " + label + ":" );
-
-		System.out.println( ">" );
-
-
-		BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
-
-		String value = null;
-
-		try {
-
-			value = input.readLine();
-		}
-
-		catch (IOException ex) { ex.printStackTrace(); }
-
-
-		return value;
 	}
 	
 	public static void print(List<BankClient> bankClients) {
